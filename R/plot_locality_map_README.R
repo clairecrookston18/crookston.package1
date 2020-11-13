96846ea74c0c99a95ac8fde643d37cedd5888b57
Claire Crookston <claire.crookston@Claires-MacBook-Pro-2.local>
2020-11-13 16:52:40 -0600

    Commiting updated version

--- R/plot_locality_map_README.R
+++ R/plot_locality_map_README.R
@@ -13,10 +13,24 @@ plot_map <- function(x) {
     select(population, long.deg, long.mins, lat.deg, lat.mins)
   pop_coords_complete <- na.rm(pop_coords)
   pop_coords_unique <- pop_coords_complete %>% 
-    distinct(long.deg, lat.deg)
+    distinct(population, long.deg, long.mins, lat.deg, lat.mins)
   pop_coords_unique_neg <- pop_coords_unique %>% 
     mutate(long.deg * (-1)) %>% 
     rename(long.deg.neg = 'long.deg * (-1)')
+  pop_coords_dec <- pop_coords_unique_neg %>% 
+    mutate(long.dec = long.mins/60) %>% 
+    mutate(lat.dec = lat.mins/60)
+  pop_coords_neg2 <- pop_coords_dec %>% 
+    mutate(long.dec * (-1)) %>% 
+    rename(long.dec.neg = 'long.dec * (-1)')
+  pop_coords_new <- pop_coords_neg2 %>% 
+    select(population, long.deg.neg, long.dec.neg, lat.deg, lat.dec)
+  pop_coords_new_comb <- pop_coords_new %>% 
+    mutate(longitude = rowSums(.[2:3])) %>%  
+    mutate(latitude = rowSums(.[4:5]))
+  just_coords_new <- pop_coords_new_comb %>% 
+    select(longitude, latitude)
+  
   library(sf)
   library(biogeo)
   library(mapview)
@@ -26,14 +40,12 @@ plot_map <- function(x) {
   library(rnaturalearthhires)
   library(maps)
   library(tools)
-  just_coords <- pop_coords_unique_neg %>% 
-    select(long.deg.neg, lat.deg)
-  just_coords_sf <- st_as_sf(just_coords, coords = c("long.deg.neg", "lat.deg"), crs = 4326, agr = "constant")
+  just_coords_new_sf <- st_as_sf(just_coords_new, coords = c("longitude", "latitude"), crs = 4326, agr = "constant")
   E_US <- ne_states(country = 'united states of america', returnclass = "sf")
   map_plot1 <- ggplot(data = E_US) +
     geom_sf() +
-    geom_sf(data = just_coords_sf, size = 4, shape = 23, fill = "darkred") +
-    coord_sf(xlim = c(-84, -81), ylim = c(33, 36), expand = FALSE)
+    geom_sf(data = just_coords_new_sf, size = 4, shape = 23, fill = "darkred") +
+    coord_sf(xlim = c(-84, -82), ylim = c(34, 36), expand = FALSE)
   assert_that(is.ggplot(map_plot1))
   return(map_plot1)
 }
